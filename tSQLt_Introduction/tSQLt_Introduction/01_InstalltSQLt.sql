bcaea02dc891a26daf7fe95553b539ec5339fb35
Roman Pijacek <33902907+RomanPijacek@users.noreply.github.com>
2020-01-04 22:34:35 +0100

    Initial scripts created

---
+++ tSQLt_Introduction/tSQLt_Introduction/01_InstalltSQLt.sql
@@ -0,0 +1,17 @@
+---------------------------------------------------------------------------------------
+-- tSQLt Framework Installation
+---------------------------------------------------------------------------------------
+
+/*
+
+	1.) The *.zip file with tSQLt can be downloaded from the following website:
+		http://tsqlt.org/
+		http://tsqlt.org/download/tsqlt/ -- direct link to the .zip file
+
+	
+	2.) Once you extract the zip file, enable CLR - execute the script [SetClrEnabled.sql]
+
+
+	3.) Install tSQLt Framework executing the [tSQLt.class.sql] script
+
+*/
