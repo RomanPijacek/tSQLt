17940637cfe461fba54a3ca8bb88f196e8ae80a1
Roman Pijacek <33902907+RomanPijacek@users.noreply.github.com>
2020-01-06 21:16:09 +0100

    Updated the SQL scripts

--- tSQLt_Introduction/tSQLt_Introduction/00_RestoreDB.sql
+++ tSQLt_Introduction/tSQLt_Introduction/00_RestoreDB.sql
@@ -7,15 +7,15 @@ GO
 
 IF DB_ID('AdventureWorks2017') IS NOT NULL
 BEGIN
-	ALTER DATABASE AdventureWorks2017 SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    ALTER DATABASE AdventureWorks2017 SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 END
 GO
 
 RESTORE DATABASE AdventureWorks2017 FROM
-	DISK = N'C:\DB_BAK\AdventureWorks2017.bak' WITH FILE = 1,  
-	MOVE N'AdventureWorks2017' TO N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSRVDEV2019\MSSQL\DATA\AdventureWorks2017.mdf',  
-	MOVE N'AdventureWorks2017_log' TO N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSRVDEV2019\MSSQL\DATA\AdventureWorks2017_log.ldf',  
-	NOUNLOAD, REPLACE, STATS = 5;
+    DISK = N'C:\DB_BAK\AdventureWorks2017.bak' WITH FILE = 1,  
+    MOVE N'AdventureWorks2017' TO N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSRVDEV2019\MSSQL\DATA\AdventureWorks2017.mdf',  
+    MOVE N'AdventureWorks2017_log' TO N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSRVDEV2019\MSSQL\DATA\AdventureWorks2017_log.ldf',  
+    NOUNLOAD, REPLACE, STATS = 5;
 GO
 
 ALTER DATABASE AdventureWorks2017 SET MULTI_USER;
---
+++ tSQLt_Introduction/tSQLt_Introduction/03_CreateNewSetUp.sql
@@ -0,0 +1,23 @@
+USE AdventureWorks2017;
+GO
+
+CREATE OR ALTER PROCEDURE [test_dbo_usp_GetLongestServingEmployee].[Setup]
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    ---------------------------------------------------------------------------------------
+    -- Fake both tables used by the procedure dbo.usp_GetLongestServingEmployee
+    ---------------------------------------------------------------------------------------
+    
+    EXECUTE tsqlt.FakeTable 
+        @TableName = N'Employee', 
+        @SchemaName = N'HumanResources',
+        @Defaults = 1;
+    
+    EXECUTE tsqlt.FakeTable 
+        @TableName = N'Person', 
+        @SchemaName = N'Person',
+        @Defaults = 1;
+END
+GO
